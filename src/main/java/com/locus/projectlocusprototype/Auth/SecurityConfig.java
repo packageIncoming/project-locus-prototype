2f735909345d4d96edd9caa81128639927af71b0
HP Z240 <mertisik329@gmail.com>
2025-12-25 22:21:24 -0700

    Set SwaggerAPI endpoint to be public (no auth)

--- src/main/java/com/locus/projectlocusprototype/Auth/SecurityConfig.java
+++ src/main/java/com/locus/projectlocusprototype/Auth/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 // set auth endpoints as public and all others as private (jwt required)
                 .authorizeHttpRequests(
                         request->
-                                request.requestMatchers("/api/auth/**","/swagger-ui/**")
+                                request.requestMatchers("/api/auth/**","/swagger-ui/**","/api-docs/**")
                                         .permitAll()
                                         .anyRequest().authenticated()
                 )
