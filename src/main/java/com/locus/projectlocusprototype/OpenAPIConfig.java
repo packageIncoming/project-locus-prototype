9b27a12a94a955a7a1122cc4574a2cfc185e0220
HP Z240 <mertisik329@gmail.com>
2025-12-25 21:42:10 -0700

    Added SwaggerAPI documentation

---
+++ src/main/java/com/locus/projectlocusprototype/OpenAPIConfig.java
@@ -0,0 +1,24 @@
+package com.locus.projectlocusprototype;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Project Locus API")
+                        .version("1.0")
+                        .description("AI-powered spaced repetition system for automated flashcard generation")
+                        .contact(new Contact()
+                                .name("Mert Isik")
+                                .email("mertisik329@gmail.com")
+                                .url("https://github.com/packageIncoming")));
+    }
+}
